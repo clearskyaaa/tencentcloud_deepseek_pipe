90fabd691f70f3e943caa9084b3c6b064c6fb390
clearskyaaa <705426085@qq.com>
2025-02-10 22:39:57 +0800

    Create pipe.py

---
+++ pipe.py
@@ -0,0 +1,148 @@
+from typing import AsyncGenerator
+from enum import Enum
+from pydantic import BaseModel, Field
+import requests
+from openai import AsyncOpenAI
+import json
+
+class State(Enum):
+    EMPTY = 1
+    THINKING = 2
+    CONTENT = 3
+
+
+class Pipe:
+    class Valves(BaseModel):
+        NAME_PREFIX: str = Field(
+            default="tencentcloud/",
+            description="Prefix to be added before model names.",
+        )
+        BASE_URL: str = Field(
+            default="https://api.lkeap.cloud.tencent.com/v1",
+            description="Base URL for accessing TencentCloud API endpoints.",
+        )
+        API_KEY: str = Field(
+            default="",
+            description="API key for authenticating requests to the TencentCloud API.",
+        )
+
+    def __init__(self):
+        self.valves = self.Valves()
+        self.state: State = State.EMPTY
+
+    def pipes(self):
+        if self.valves.API_KEY:
+            try:
+                if not self.valves.API_KEY:
+                    raise Exception("no api key")
+                headers = {
+                    "Authorization": f"Bearer {self.valves.API_KEY}",
+                    "Content-Type": "application/json",
+                }
+
+                r = requests.get(
+                    f"{self.valves.BASE_URL}/models", headers=headers
+                )
+                models = r.json()
+
+                return [
+                    {
+                        "id": model["id"],
+                        "name": f'{self.valves.NAME_PREFIX}{model.get("name", model["id"])}',
+                    }
+                    for model in models["data"]
+                ]
+            except Exception as e:
+                return [
+                    {
+                        "id": "error",
+                        "name": "Error fetching models. Please check your API Key."
+                    },
+                ]
+        else:
+            return [
+                {
+                    "id": "error",
+                    "name": "API Key not provided.",
+                },
+            ]
+
+    @staticmethod
+    def generate_json_error(err_msg:str):
+        return '''```json
+{{
+  "error": "{}",
+}}
+```\n'''.format(err_msg)
+
+    async def pipe(self, body: dict) ->AsyncGenerator[str, None]:
+        self.state = State.EMPTY
+        try:
+            async with (AsyncOpenAI(api_key=self.valves.API_KEY, base_url=self.valves.BASE_URL) as client):
+                model = body["model"]
+                model_s = model.split(".")
+                model_id = model_s[len(model_s) - 1]
+                chat_completion = await client.chat.completions.create(
+                    model=model_id,
+                    messages=body["messages"],
+                    stream=True
+                )
+                response = chat_completion.response
+                iter = response.aiter_lines()
+                first_line = await iter.__anext__()
+                try:
+                    first_line_json = json.loads(first_line)
+                    if 'error' in first_line_json and 'message' in first_line_json['error']:
+                        yield self.generate_json_error(first_line_json['error']['message'])
+                        return
+                except:
+                    pass
+
+                decoder = client._make_sse_decoder()
+                decoder.decode(first_line)
+                async for line in iter:
+                    sse = decoder.decode(line)
+                    if not sse:
+                        continue
+                    if sse.data.startswith("[DONE]"):
+                        break
+                    sse_json = sse.json()
+                    if 'choices' in sse_json and isinstance(sse_json['choices'], list) \
+                        and len(sse_json['choices']) > 0 and 'delta' in sse_json['choices'][0]:
+                        delta = sse_json['choices'][0]['delta']
+                        if 'reasoning_content' in delta:
+                            reasoning_content = delta['reasoning_content']
+                            if reasoning_content:
+                                if self.state != State.THINKING:
+                                        self.state = State.THINKING
+                                        yield "<think>"
+                                yield reasoning_content
+                        if 'content' in delta:
+                            content = delta['content']
+                            if content:
+                                if self.state == State.THINKING:
+                                        self.state = State.CONTENT
+                                        yield "</think>"
+                                yield content
+        except Exception as e:
+            yield self.generate_json_error(str(e))
+        return
+
+
+if __name__ == '__main__':
+    async def main():
+        body = {
+            "model": "test.deepseek-r1",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "你好",
+                }
+            ],
+        }
+        pipe = Pipe()
+        async for item in pipe.pipe(body):
+            print(item, end="")
+
+    import asyncio
+    asyncio.run(main())
